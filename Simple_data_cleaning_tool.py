8ff86a9de6dfa8a0b69a9c77805d46a6adf25b83
mayaannkkk <goyalmayank492@gmail.com>
2025-11-12 00:16:33 +0530

    Project

--- Simple_data_cleaning_tool.py
+++ Simple_data_cleaning_tool.py
@@ -5,12 +5,12 @@ list1 = []
 for i in range(len(data_list)):
     # Remove special character from a word in a list
     data_list[i] = re.sub(r'[^A-Za-z0-9 ]', '', data_list[i])
-    # Remove
+    # strip leading/trailing spaces
     data_list[i] = data_list[i].strip()
     # Remove empty values
     if data_list[i] == "" or data_list[i] == " ":
         continue
-        # remove multiple spaces in the middle
+    # Remove multiple spaces in the middle
     data_list[i] = " ".join(data_list[i].split())
     # Remove duplicates
     if data_list[i] in list1:
